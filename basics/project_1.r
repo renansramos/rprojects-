081185d3f787b9caa7ac21d0ab93b65d8d1d8553
Renan Ramos <86578128+renansramos@users.noreply.github.com>
2023-12-03 16:41:42 -0300

    Create project_1.r

---
+++ basics/project_1.r
@@ -0,0 +1,7 @@
+print("Hello World")
+#Jogo da velha é usado para comentários em R. 
+
+  variavel_one <- "My first Variable in RR"
+  variavel_two <- 50
+  vec_1 <-c(10, 16.4, 28.5,7,3)
+  print("vec_1")
